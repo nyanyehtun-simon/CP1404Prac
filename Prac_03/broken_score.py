1ee8d1aecf27f9895a5512434b075b37b8653c76
Nyanye htun <nyanyehtun@thinkbluedata.com>
2018-11-20 11:46:23 +0800

    Refactor code for broken_score.py and temperatures.py

--- Prac_03/broken_score.py
+++ Prac_03/broken_score.py
@@ -1,11 +1,20 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif 0 <= score <= 100:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    elif score < 50:
-        print("Bad")
+def main():
+    score = float(input("Enter score: "))
+
+    result = determine_score(score)
+    print(result)
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif 0 <= score <= 100:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        elif score < 50:
+            return "Bad"
+
+main()
+
 

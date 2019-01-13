5c32fcb689c7afd647b81ec37758e7da21b011e8
Nyanye htun <nyanyehtun@my.jcu.edu.au>
2019-01-14 04:11:34 +0800

    Remove unnecessary code

--- Prac_09/files_to_sort_v2.py
+++ Prac_09/files_to_sort_v2.py
@@ -56,29 +56,4 @@ def main():
                 # just move the file to new folder location
                 shutil.move(filename, folder_name + '/' + filename)
 
-    return
-
-    # Change to desired directory
-    # os.chdir('FilesToSort')
-
-    existing_dirs = []
-
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            existing_dirs.append(filename)
-            continue
-
-        extension = filename.split('.')[1]
-        if extension not in existing_dirs:
-            # create a new dir
-            try:
-                os.mkdir(extension)
-                existing_dirs.append(extension)
-            except FileExistsError:
-                print('File already exists')
-
-        # just move the file to new folder location
-        shutil.move(filename, extension + '/' + filename)
-
 main()
